040b68f89d0c18dbaf1f20b314d59e44fd9cfa8b
JulianH99 <juliancorredors99@outlook.com>
2018-06-21 08:51:12 -0500

    Commit inicial

---
+++ script.sql
@@ -0,0 +1,15 @@
+create database if not exists pjerrejerre;
+
+use pjerrejerre;
+
+
+-- tabla correspondiente a los usuarios
+create table if not exists usuarios (
+    id int auto_increment,
+    email varchar(30) not null,
+    nombre varchar(15) null,
+    apellido varchar(15) null,
+    estado smallint not null
+    primary key(id)
+
+);
